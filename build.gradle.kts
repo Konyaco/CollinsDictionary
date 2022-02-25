df2356a2e6c54234b34a3d6318bf5b6c4414db02
Konyaco <atwzj233@gmail.com>
2022-02-25 09:04:57 +0800

    [Code] Update compose version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("com.android.library") version "7.2.0-alpha03" apply false
     kotlin("android") version "1.6.10" apply false
     kotlin("multiplatform") version "1.6.10" apply false
-    id("org.jetbrains.compose") version "1.1.0-rc01" apply false
+    id("org.jetbrains.compose") version "1.1.0" apply false
     kotlin("plugin.serialization") version "1.6.10" apply false
 }
 
