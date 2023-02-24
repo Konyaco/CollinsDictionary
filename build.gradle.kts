1006a153157219294289550c67dd09cf557e715e
KonYaco <atwzj233@gmail.com>
2023-02-24 13:47:36 +0800

    Update dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("com.android.library") version "7.3.1" apply false
     kotlin("android") version "1.7.20" apply false
     kotlin("multiplatform") version "1.7.20" apply false
-    id("org.jetbrains.compose") version "1.2.0" apply false
+    id("org.jetbrains.compose") version "1.3.0" apply false
     kotlin("plugin.serialization") version "1.7.20" apply false
 }
 
